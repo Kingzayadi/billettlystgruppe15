528f3992e9636521527c890d9017738c97b78f5f
SajadZayadi <Sajadadel123@hotmail.com>
2025-05-18 22:55:09 +0200

    La til kommentarer på eventcard.jsx med kilde.

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -5,13 +5,16 @@ function EventCard({ event }) {
   return (
     <div className="event-card">
       <img
+      //bruker optional chaining for å være 100% sikker på at bildet finnes i dataen.
         src={event.images?.[0]?.url}
         alt={event.name}
         className="event-image"
       />
       <div className="event-info">
         <h3>{event.name}</h3>
+        {/* viser navnet på stedet hvis det finnes */}
         <p>{event._embedded?.venues?.[0]?.name}</p>
+        {/* navigerer til detaljer om arrangementet. kilde: https://reactrouter.com/en/main/components/link */}
         <Link to={`/event/${event.id}`} className="details-link">
           Se mer detaljer
         </Link>
