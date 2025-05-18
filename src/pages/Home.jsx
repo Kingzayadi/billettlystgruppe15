07f5e5fedf805538be287758af0a3e83f04b97b8
SajadZayadi <Sajadadel123@hotmail.com>
2025-05-18 21:09:47 +0200

    la til kommentarer i home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,17 +9,21 @@ function Home() {
 
   useEffect(() => {
     const fetchFestivalEvents = async () => {
+      // Liste over festivaler som skal vises. Her har jeg brukt andre navn siden det ikke kom opp på de 4 som lo i eksamenteksten for å vise at jeg vet hva den oppgaven går ut over
+      // Ligger mer forklaring i rapportdokumentet.
       const names = ["Roskilde", "Midgardsblot", "Alive Festival", "Heartland"];
       const results = [];
       const notFound = [];
 
       for (const name of names) {
         try {
+          //Dette er en ekstern kilde jeg har brukt. Jeg brukte da corsproxy.io for å unngå CORS problemer med API.
           const response = await fetch(
             `https://corsproxy.io/?https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&countryCode=NO&keyword=${encodeURIComponent(name)}`
           );
           const data = await response.json();
 
+          //det blir hentet første arrangament her som matcher da navnet
           if (data._embedded?.events?.length > 0) {
             results.push(data._embedded.events[0]);
           } else {
@@ -27,6 +31,7 @@ function Home() {
             notFound.push(name);
           }
         } catch (err) {
+          //feilmelding hvis det skjer et problem med fetch
           console.error(`Feil ved henting av ${name}:`, err);
           notFound.push(name);
         }
