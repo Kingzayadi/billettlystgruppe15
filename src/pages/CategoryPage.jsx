f8861237ab34c989ec03aacb447f8031d493c4f3
SajadZayadi <Sajadadel123@hotmail.com>
2025-05-18 22:48:30 +0200

    Kommentert categorypage.jsx. Ikke brukt kilder fordi jeg har brukt generelle kunnskap. webtricks LMS hjalp

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+//brukt for å konvertere det til keywords med api-søk.
 const categoryKeywords = {
   musikk: "concert",
   sport: "sports",
@@ -9,20 +10,22 @@ const categoryKeywords = {
 };
 
 function CategoryPage() {
-  const { slug } = useParams();
-  const [events, setEvents] = useState([]);
+  const { slug } = useParams(); //brukt for å hente dynamisk kategori
+  const [events, setEvents] = useState([]); //liste for events
 
   useEffect(() => {
-    const keyword = categoryKeywords[slug] || slug;
+    const keyword = categoryKeywords[slug] || slug; //for å hente riktig søkeord for API
 
     const fetchCategoryEvents = async () => {
       try {
         const response = await fetch(
+          //skulle egentlig bruke CORS proxy men ble droppet.
           `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.REACT_APP_TM_API_KEY}&keyword=${encodeURIComponent(keyword)}&countryCode=NO`
         );
 
         const data = await response.json();
 
+        //if kode hvis events ikke finnes i resultatet.
         if (data._embedded && data._embedded.events) {
           setEvents(data._embedded.events);
         } else {
@@ -39,7 +42,9 @@ function CategoryPage() {
 
   return (
     <main>
+      {/* vises valgt kategori som overskrift */}
       <h1>{slug.toUpperCase()}</h1>
+      {/* vises events eller feilmelding hvis det ikke finnes */}
       <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
         {events.length > 0 ? (
           events.map((event) => (
