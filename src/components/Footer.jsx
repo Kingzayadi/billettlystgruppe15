116435f2039ca13ca3fe37e1e71a565276474124
SajadZayadi <Sajadadel123@hotmail.com>
2025-05-18 22:09:45 +0200

    Kommentert footer.jsx. brukt vanlig react.jsx syntaks for html.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 function Footer() {
     return (
       <footer style={{ textAlign: "center", marginTop: "3rem", padding: "1rem 0", fontSize: "0.9rem", color: "#555" }}>
+        {/*attribution til ticketmaster API */}
         Data hentet fra{" "}
         <a href="https://developer.ticketmaster.com/" target="_blank" rel="noopener noreferrer">
           Ticketmaster API
